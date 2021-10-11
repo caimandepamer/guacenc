cd3db43a53f4fe65eadc3d5d434a8af6871d08e6
caimandepamer <rafael.campoverde.mariscal@gmail.com>
2021-10-11 00:33:10 -0300

    Create convert.sh

---
+++ convert.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+size=$size
+bits=$bits
+#========== eval size env ======================
+if [ "$size" == "" ] ; then 
+	size=1920x1080;
+fi
+#========== eval bits env ======================
+if [ "$bits" == "" ] ; then 
+	bits=2000000;
+fi
+#========== eval if /record/converted exists ===
+WORK=/record
+if [ ! -d "$WORK" ]; then echo "No esta mintado /record";exit 1; fi
+DIR=/record/converted
+if [ ! -d "$DIR" ]; then echo "creando $DIR"; mkdir -p $DIR; fi
+#=========== start the convertion of ===========
+#=========== all files in /record    ===========
+for file in $(ls $WORK | grep -v converted | grep -vE  ".m4v$"); do
+	echo "/usr/local/bin/guacenc -s $size -r $bits $WORK/$file";
+	/usr/local/bin/guacenc -s $size -r $bits "$WORK/$file";
+	mv "$WORK/$file".m4v "$DIR"/
+	mv "$WORK/$file" "$DIR"/
+	echo "$file converted."
+done
