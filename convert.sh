183a0a528a5229297fa7cd56ff68e658470271a3
caimandepamer <rafael.campoverde.mariscal@gmail.com>
2021-11-13 15:10:51 -0300

    comilla en linea 25

--- convert.sh
+++ convert.sh
@@ -19,7 +19,7 @@ if [ ! -d "$DIR" ]; then echo "creando $DIR"; mkdir -p $DIR; fi
 for file in $(ls $WORK | grep -v converted | grep -vE  ".m4v$"); do
 	echo "/usr/local/bin/guacenc -s $size -r $bits $WORK/$file";
 	/usr/local/bin/guacenc -s $size -r $bits "$WORK/$file";
-	/usr/bin/ffmpeg -i $WORK/$file".m4v "$DIR"/"$file".webm
+	/usr/bin/ffmpeg -i "$WORK/$file".m4v "$DIR"/"$file".webm
 	mv "$WORK/$file".m4v "$DIR"/
 	mv "$WORK/$file" "$DIR"/
 	echo "$file converted to m4v and webm"
